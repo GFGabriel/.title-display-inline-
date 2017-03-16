81f2e80dc5ea1889c385f19c973eecd1dc751e91
George Flauta <george.flauta@colorado.edu>
2017-03-15 22:38:14 -0600

    :sparkles: Added Username, Scoring, Scoreboard, and Game Over Screen

---
+++ buttons.js
@@ -0,0 +1,59 @@
+var buttons = [
+  {
+    name: "strongOpen",
+    innerText: "&lt;strong&gt;",
+    classes: ["html", "drag", "button"],
+    tag: "opening",
+    type: "strong",
+  },
+  {
+    name: "strongClose",
+    innerText: "&lt;/strong&gt;",
+    classes: ["html", "drag", "button"],
+    tag: "closing",
+    type: "strong",
+  },
+  {
+    name: "emOpen",
+    innerText: "&lt;em&gt;",
+    classes: ["html", "drag", "button"],
+    tag: "opening",
+    type: "em",
+  },
+  {
+    name: "emClose",
+    innerText: "&lt;/em&gt;",
+    classes: ["html", "drag", "button"],
+    tag: "closing",
+    type: "em",
+  },
+  {
+    name: "aOpen",
+    innerText: "&lt;a&gt;",
+    classes: ["html", "drag", "button"],
+    tag: "opening",
+    type: "a",
+  },
+  {
+    name: "aClose",
+    innerText: "&lt;/a&gt;",
+    classes: ["html", "drag", "button"],
+    tag: "closing",
+    type: "a",
+  },
+  {
+    name: "color",
+    innerText: "color:",
+    classes: ["css", "drag", "button"],
+    tag: "keyword",
+    type: "purple",
+  },
+  {
+    name: "purple",
+    innerText: "purple",
+    classes: ["css", "drag", "button"],
+    tag: "value",
+    type: "purple",
+  }
+
+]
--- main.js
+++ main.js
@@ -2,101 +2,91 @@ $( document ).ready(function() {
 	var currentClasses = $('#currentStatus')[0].classList
 	var level = 0
 	var pairNum = 0
+	var username
 
-	initLevel(level)
+	console.log(score)
 
+	getLeaderboard()
+	initLevel(level, score)
 
-   $('.drag').draggable({
-			helper: "clone",
-			revert: 'invalid',
-			snap: true,
-			snapMode: "inner",
-			snapTolerance: 30
-		});
+	function scoreTimer(x) {
+		var seconds = x;
+		var currentClasses = $('#currentStatus')[0].classList
+		var targetClasses = $('#targetStatus')[0].classList
+		 function tick() {
+				 var counter = document.getElementById("countdown");
+				 seconds--;
+				 counter.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds);
+				 if ($(targetClasses).not(currentClasses).length === 0 && $(currentClasses).not(targetClasses).length === 0) {
+					 score += seconds
+					 console.log(score);
+					 return;
+				 } else if( seconds > 0 ) {
+						 setTimeout(tick, 1000);
+						 console.log(seconds)
+				 } else {
+						 gameOver();
+				 }
+		 }
+		 console.log(seconds);
+		 tick();
+	}
 
-		$('.htmlBucket').droppable({
-			accept: ".html",
-			drop: function (event, ui) {
-				$(this).append(ui.draggable)
-				//set variables for checking the two windows to see if they are the same
-				var currentClasses = $('#currentStatus')[0].classList
-				var targetClasses = $('#targetStatus')[0].classList
-				//get current pair number
-				var currentPair = $(this).attr('data-pair')
-				//get the buckets that match the current pair and separate them
-				var pairBuckets = $('[data-pair="' + currentPair + '"]')
-				var firstOfBuckets = pairBuckets[0]
-				var secondOfBuckets = pairBuckets[1]
-				//get the buttons inside of those buckets and separate them
-				var pairButtons = $('[data-pair="' + currentPair + '"]').find("div")
-				var firstOfButtons = pairButtons[0]
-				var secondOfButtons = pairButtons[1]
-				//check if both buckets have buttons
-				if (pairButtons[0] != undefined && pairButtons[1] != undefined) {
-					//check if the opening tag is first and closing tag is second
-					if ($(firstOfBuckets).data("tag") == $(firstOfButtons).data("tag") && $(secondOfBuckets).data("tag") == $(secondOfButtons).data("tag")) {
-						//check if the data-types match of the two buttons
-						if ($(firstOfButtons).data("type") == $(secondOfButtons).data("type")) {
-							//create new class to add to current status window
-	    				var newStatus = $(secondOfButtons).data("type") + 'Text'
-							//add the created class
-							$('#currentStatus').addClass(newStatus)
-						}
-					}
-				}
-				//check to see if the two windows are the same and show win condition window
-				if ($(targetClasses).not(currentClasses).length === 0 && $(currentClasses).not(targetClasses).length === 0) {
-					$( "#congrats" ).dialog( "open" )
-				}
-			}
-		})
+	$("#tutorial").dialog({
+		width: 565,
+		dialogClass: "no-close",
+		autoOpen: true,
+		modal: true,
+		show: {
+			effect: "bounce",
+			duration: 1000
+		},
+		hide: {
+			effect: "clip",
+			duration: 50
+		},
+		buttons: [
+			{
+				text: "Ok, I got it!",
+				click: function() {
+					$( this ).dialog( "close" );
+					$("#nameRequest").dialog({
+						width: 565,
+						dialogClass: "no-close",
+						autoOpen: true,
+						modal: true,
+						show: {
+							effect: "bounce",
+							duration: 1000
+						},
+						hide: {
+							effect: "clip",
+							duration: 50
+						},
+						buttons: [
+							{
+								text: "Let's get started!",
+								click: function() {
+									username = $('#username').val()
+									if (username.length < 2) {
+										username = "The Enigma"
+									}
+									$('#showUsername').text(username);
+									$( this ).dialog( "close" );
+									scoreTimer(60, score)
 
-		$(".cssBucket").droppable({
-			accept: ".css",
-			drop: function (event, ui) {
-				$(this).append(ui.draggable)
-
-				//set variables for checking the two windows to see if they are the same
-				var currentClasses = $('#currentStatus')[0].classList
-				var targetClasses = $('#targetStatus')[0].classList
-				//get current pair number
-				var currentPair = $(this).attr('data-pair')
-				//get the buckets that match the current pair and separate them
-				var pairBuckets = $('[data-pair="' + currentPair + '"]')
-				var firstOfBuckets = pairBuckets[0]
-				var secondOfBuckets = pairBuckets[1]
-				//get the buttons inside of those buckets and separate them
-				var pairButtons = $('[data-pair="' + currentPair + '"]').find("div")
-				var firstOfButtons = pairButtons[0]
-				var secondOfButtons = pairButtons[1]
-				//check if both buckets have buttons
-				if (pairButtons[0] != undefined && pairButtons[1] != undefined) {
-					//check if the opening tag is first and closing tag is second
-					if ($(firstOfBuckets).data("tag") == $(firstOfButtons).data("tag") && $(secondOfBuckets).data("tag") == $(secondOfButtons).data("tag")) {
-						//check if the data-types match of the two buttons
-						if ($(firstOfButtons).data("type") == $(secondOfButtons).data("type")) {
-							//create new class to add to current status window
-							var newStatus = $(secondOfButtons).data("type") + 'Text'
-							//add the created class
-							$('#currentStatus').addClass(newStatus)
-						}
-					}
-				}
-				//check to see if the two windows are the same and show win condition window
-				if ($(targetClasses).not(currentClasses).length === 0 && $(currentClasses).not(targetClasses).length === 0) {
-					$( "#congrats" ).dialog( "open" )
+								}
+							}
+						]
+					})
 				}
 			}
-		})
-
-		$('#htmlTags').droppable({
-				drop: function (event, ui) {
-					$(this).append(ui.draggable)
-				}
-		})
+		]
+	})
 
 		$("#congrats").dialog({
 			width: 565,
+			dialogClass: "no-close",
       autoOpen: false,
       show: {
         effect: "bounce",
@@ -105,23 +95,157 @@ $( document ).ready(function() {
       hide: {
         effect: "clip",
         duration: 50
-      }
-    });
-
-
+      },
+			buttons: [
+				{
+					text: "End Game?",
+					click: function() {
+        		$( this ).dialog( "close" );
+      		}
+				},
+				{
+					text: "Next Level?",
+					click: function() {
+						score += 100
+						level += 1
+						// console.log(level);
+						initLevel(level, score)
+						$(this).dialog( "close" )
+						scoreTimer(60, score)
+					}
+				}
+			]
+    })
 });
 
 var genericText = "<section> This is some text.</section>"
 var prependBucket = "<div class=\"bucket htmlBucket\" data-tag=\"opening\"></div>"
 var appendBucket = "<div class=\"bucket htmlBucket\" data-tag=\"closing\"></div>"
+var score = 0
 
-function initLevel (level) {
+
+function initLevel (level, score) {
+	console.log(score);
+	clearLevel()
 	createHTMLBox(levels[level].textLines, levels[level].htmlBuckets)
 	createCSSBox(levels[level].cssBlocks, levels[level].cssSelectors, levels[level].cssPairs)
 	createCurrentStatus(levels[level].textLines)
 	createTargetStatus(levels[level].textLines, levels[level].classesNeeded)
+	createButtons (levels[level].buttonsNeeded)
+	makeDraggable()
+	makeHTMLDroppable()
+	makeCSSDroppable()
+	makeButtonBoxDroppable()
+	updateScore (score)
 	pairNum = 0;
 }
+
+function makeDraggable() {
+	$('.drag').draggable({
+		 helper: "clone",
+		 revert: 'invalid',
+		//  snap: true,
+		//  snapMode: "inner",
+		//  snapTolerance: 20
+	});
+}
+
+function makeHTMLDroppable() {
+	$('.htmlBucket').droppable({
+		accept: ".html",
+		drop: function (event, ui) {
+			$(this).append(ui.draggable)
+			//set variables for checking the two windows to see if they are the same
+			var currentClasses = $('#currentStatus')[0].classList
+			var targetClasses = $('#targetStatus')[0].classList
+			//get current pair number
+			var currentPair = $(this).attr('data-pair')
+			//get the buckets that match the current pair and separate them
+			var pairBuckets = $('[data-pair="' + currentPair + '"]')
+			var firstOfBuckets = pairBuckets[0]
+			var secondOfBuckets = pairBuckets[1]
+			//get the buttons inside of those buckets and separate them
+			var pairButtons = $('[data-pair="' + currentPair + '"]').find("div")
+			var firstOfButtons = pairButtons[0]
+			var secondOfButtons = pairButtons[1]
+			//check if both buckets have buttons
+			if (pairButtons[0] != undefined && pairButtons[1] != undefined) {
+				//check if the opening tag is first and closing tag is second
+				if ($(firstOfBuckets).data("tag") == $(firstOfButtons).data("tag") && $(secondOfBuckets).data("tag") == $(secondOfButtons).data("tag")) {
+					//check if the data-types match of the two buttons
+					if ($(firstOfButtons).data("type") == $(secondOfButtons).data("type")) {
+						//create new class to add to current status window
+						var newStatus = $(secondOfButtons).data("type") + 'Text'
+						//add the created class
+						$('#currentStatus').addClass(newStatus)
+					}
+				}
+			}
+			//check to see if the two windows are the same and show win condition window
+			if ($(targetClasses).not(currentClasses).length === 0 && $(currentClasses).not(targetClasses).length === 0) {
+				$( "#congrats" ).dialog( "open" )
+			}
+		}
+	})
+}
+
+function makeCSSDroppable() {
+	$(".cssBucket").droppable({
+		accept: ".css",
+		drop: function (event, ui) {
+			$(this).append(ui.draggable)
+
+			//set variables for checking the two windows to see if they are the same
+			var currentClasses = $('#currentStatus')[0].classList
+			var targetClasses = $('#targetStatus')[0].classList
+			//get current pair number
+			var currentPair = $(this).attr('data-pair')
+			//get the buckets that match the current pair and separate them
+			var pairBuckets = $('[data-pair="' + currentPair + '"]')
+			var firstOfBuckets = pairBuckets[0]
+			var secondOfBuckets = pairBuckets[1]
+			//get the buttons inside of those buckets and separate them
+			var pairButtons = $('[data-pair="' + currentPair + '"]').find("div")
+			var firstOfButtons = pairButtons[0]
+			var secondOfButtons = pairButtons[1]
+			//check if both buckets have buttons
+			if (pairButtons[0] != undefined && pairButtons[1] != undefined) {
+				//check if the opening tag is first and closing tag is second
+				if ($(firstOfBuckets).data("tag") == $(firstOfButtons).data("tag") && $(secondOfBuckets).data("tag") == $(secondOfButtons).data("tag")) {
+					//check if the data-types match of the two buttons
+					if ($(firstOfButtons).data("type") == $(secondOfButtons).data("type")) {
+						//create new class to add to current status window
+						var newStatus = $(secondOfButtons).data("type") + 'Text'
+						//add the created class
+						$('#currentStatus').addClass(newStatus)
+					}
+				}
+			}
+			//check to see if the two windows are the same and show win condition window
+			if ($(targetClasses).not(currentClasses).length === 0 && $(currentClasses).not(targetClasses).length === 0) {
+				$( "#congrats" ).dialog( "open" )
+			}
+		}
+	})
+}
+
+function makeButtonBoxDroppable() {
+	$('#htmlTags').droppable({
+			drop: function (event, ui) {
+				$(this).append(ui.draggable)
+			}
+	})
+}
+
+function clearLevel() {
+	$('.htmlCodeBox').empty()
+	$('.cssCodeBox').empty()
+	$('#currentStatus').empty()
+	$('#currentStatus')[0].classList = ""
+	$('#targetStatus').empty()
+	$('#targetStatus')[0].classList = ""
+}
+
 function createHTMLBox(x, y) {
 	for (var i = 0; i < x; i++) {
 		$(genericText).appendTo('.htmlCodeBox')
@@ -167,7 +291,7 @@ function createCSSBox(x, y, z) {
 
 function createCSSPairs(x) {
 	var n = pairNum
-	console.log(n)
+	// console.log(n)
 	for (var i = n; i < x + n; i++) {
 		var cssPair = "<div class=\"cssPair\"><div class=\"bucket cssBucket\" data-tag=\"keyword\" data-pair=" + i + "></div><!-- --><div class=\"bucket cssBucket\" data-tag=\"value\" data-pair=" + i + "></div></div>"
 		$(cssPair).appendTo('.cssCodeBox')
@@ -194,9 +318,105 @@ function addTargetClasses(x) {
 	}
 }
 
+function createButtons (x) {
+	for (var i = 0; i < x.length; i++) {
+		searchButtons(x[i])
+	}
+}
+
+function searchButtons (name) {
+	for (var i = 0; i < buttons.length; i++) {
+		if (buttons[i].name == name) {
+			var buttonClasses = createClasses(buttons[i].classes)
+
+			var strongButton = "<div class=\"" + buttonClasses + "\"  data-tag=\"" + buttons[i].tag + "\" data-type=\"" + buttons[i].type + "\">" + buttons[i].innerText + "</div>"
+			$(strongButton).appendTo('#htmlTags')
+		}
+	}
+}
+
+function createClasses (buttonClasses) {
+	var classString = ""
+	for (var i = 0; i < buttonClasses.length; i++) {
+		if (i == buttonClasses.length - 1) {
+			classString += buttonClasses[i]
+		} else {
+			classString += buttonClasses[i] + " "
+		}
+	}
+	return classString
+}
+
 function checkHTMLMatch() {
 
 }
+
+function getLeaderboard() {
+	$.get("https://galvanize-leader-board.herokuapp.com/api/v1/leader-board/Space").then (function(data) {
+		// console.log(data)
+		var leaderboard = "<p>" + data[0].player_name + ": " + data[0].score + "</p> <p>" + data[1].player_name + ": " + data[1].score + "</p> <p>" + data[2].player_name + ": " + data[2].score + "</p>"
+		$(leaderboard).appendTo(".leaderboard")
+
+		// div class=\"" + buttonClasses + "\"  data-tag=\"" + buttons[i].tag + "\" data-type=\"" + buttons[i].type + "\">" + buttons[i].innerText + "</div>"
+	})
+}
+
+// function scoreTimer(x, score) {
+// 	var seconds = x;
+// 	var currentClasses = $('#currentStatus')[0].classList
+// 	var targetClasses = $('#targetStatus')[0].classList
+// 	 function tick() {
+// 			 var counter = document.getElementById("countdown");
+// 			 seconds--;
+// 			 counter.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds);
+// 			 if ($(targetClasses).not(currentClasses).length === 0 && $(currentClasses).not(targetClasses).length === 0) {
+// 				 score += seconds
+// 				 return;
+// 			 } else if( seconds > 0 ) {
+// 					 setTimeout(tick, 1000);
+// 			 } else {
+// 					 gameOver();
+// 			 }
+// 	 }
+// 	 tick();
+// }
+
+function updateScore (score) {
+		$('#scoreBox').text("Score: " + score);
+}
+
+function gameOver() {
+	$("#gameOver").dialog({
+		width: 755,
+		dialogClass: "no-close",
+		autoOpen: false,
+		show: {
+			effect: "bounce",
+			duration: 1000
+		},
+		hide: {
+			effect: "clip",
+			duration: 50
+		},
+		buttons: [
+			{
+				text: "End Game?",
+				click: function() {
+					$( this ).dialog( "close" );
+				}
+			},
+			{
+				text: "Restart?",
+				click: function() {
+					location.reload(true);
+				}
+			}
+		]
+	})
+	$( "#gameOver" ).dialog( "open" )
+}
+
+
 //
 // var firstBucketClassList =
 // tags = {
